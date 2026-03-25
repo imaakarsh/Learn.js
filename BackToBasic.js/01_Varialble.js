74645b320f892c326e60a72eac08fe5082dfe454
imaakarsh <aakarsh027@outlook.com>
2026-03-25 17:44:40 +0530

    feat: add multiple JavaScript files demonstrating variables, data types, arrays, objects, and methods

---
+++ BackToBasic.js/01_Varialble.js
@@ -0,0 +1,18 @@
+const account_Id = 122344
+let accountEmail = "Aakarsh@gmail.com"
+var accountPassward = "2345"
+accountCity = "Uttarpredesh"
+let accountState;
+// account_Id = 2 // Not allowed
+
+accountEmail = "hdds@gamil.com"
+accountPassward =6744
+console.log(account_Id);
+console.log(accountEmail);
+
+/*
+Prefer not to use var 
+because of issue in block 
+*/
+console.table([account_Id,accountEmail,accountPassward,accountCity,accountState])
+
---
+++ BackToBasic.js/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // We are using nodejs, not browser
+
+console.log(3+3); //code readability shouled be high 
+
+console.log("Aakarsh");
+
