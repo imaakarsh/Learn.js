68b21d540708b2e18a7d35e833444da314992edb
imaakarsh <aakarsh027@outlook.com>
2026-03-25 18:29:51 +0530

    fix: comment out console.log statements for cleaner output in 03_ConversionOperation.js

--- BackToBasic.js/03_ConversionOperation.js
+++ BackToBasic.js/03_ConversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "aakarsh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false 
 // "" => false 
@@ -22,5 +22,36 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 22
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// --------- Operation ---------
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+3);
+
+let str1 = "hello"
+let str2 = " aakarsh"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
