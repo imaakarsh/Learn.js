8954a78bfe0677ec48dd1adab6a6ef40fb2d36cd
imaakarsh <aakarsh027@outlook.com>
2026-03-25 17:52:23 +0530

    feat: add variable declarations and data type comments in 02_datatypes.js

--- BackToBasic.js/02_datatypes.js
+++ BackToBasic.js/02_datatypes.js
@@ -6,3 +6,21 @@ console.log(3+3); //code readability shouled be high
 
 console.log("Aakarsh");
 
+let name = "Aakarsh"
+let age = 18;
+let isLoggedIn = false
+let state;
+
+// number => 2 to pow 53
+// bigint
+// string  => ""
+// boolean true/false
+// null  => standalone value
+// undefined => 
+// symbol => uniques
+
+// object 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
