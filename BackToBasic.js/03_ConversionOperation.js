5be14fed0e4456871f6f1da22833e437c83d7d56
imaakarsh <aakarsh027@outlook.com>
2026-03-25 18:15:44 +0530

    feat: add conversion operations for boolean and number types in 03_ConversionOperation.js

---
+++ BackToBasic.js/03_ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = true;
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeofvalueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abd" => NaN
+// "true" => 1; false => 0
+
+let isLoggedIn = "aakarsh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false 
+// "" => false 
+// "aakarsh" => true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
