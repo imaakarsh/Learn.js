56564c4b2f433c95e9013159fb847953020b54b3
imaakarsh <aakarsh027@outlook.com>
2026-04-29 02:03:57 +0530

    feat: add variable declarations and initial values in 01_Variables.js

---
+++ JavaScript/01_Variables.js
@@ -0,0 +1,16 @@
+const account_Id = 1543 // not allowed to change
+let accountEmail = "aakarsh@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState 
+accountEmail = "aakarsj"
+accountPassword = "q334r"
+// console.log(accountPassword);
+
+
+console.table([account_Id,accountEmail,accountCity,accountState]);
+
+
+
+// prefernot to use var bcz of issue 
+
