70f8a84da0f0927a05449ad7ab69bea3d23a4519
imaakarsh <aakarsh027@outlook.com>
2026-05-24 19:21:25 +0530

    chore: comment out initial code and maintain structure for future reference

--- Namaste JavaScript/index.js
+++ Namaste JavaScript/index.js
@@ -1,10 +1,29 @@
-console.log("Hello dunia");
+// console.log("Hello dunia");
 
 
-function solve(){
-    var a = 12
-    console.log(a);
+// function solve(){
+//     var a = 12
+//     console.log(a);
     
-}
+// }
 
-solve()
+// solve()
+
+
+let x  = 4
+
+x = "aakarsh"
+const a  = 34
+
+a = 23
+console.log(a);
+
+const valueAarr = 344
+console.log(valueAarr);
+
+var ValueOfStudent = 234
+ValueOfStudent = 344
+
+console.log(ValueOfStudent);
+
+
