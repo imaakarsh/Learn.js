d614276bd3f511b98ed9f1833b00191659b6637d
imaakarsh <aakarsh027@outlook.com>
2026-05-25 19:55:42 +0530

    feat: add initial HTML, CSS, and JavaScript files for the project; remove unused index.js

---
+++ JavaScriptEverything/script.js
@@ -0,0 +1,28 @@
+// var let const - line by line 
+
+// var a
+// var a = 2;
+// let a;
+// let a=23
+// var UserName = "Aakarsh"
+// a = 23;
+// console.log(UserName);
+// console.log(a);
+
+// declaraton and initialization
+// var a; //decelare
+// var a = 3; //decelare and init
+
+// window mein add hota hai
+// function scoped hota hai
+
+// app fhirse decelar kr skte ho error nhi aayega
+
+// var a = 13;
+// let a = 13;
+
+// Scope (global,block,functional)
+
+
+// reassignemet , redeclaration (possible with var)
+
