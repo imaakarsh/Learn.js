7c7a666599c1c90c2f51bce04684a3e8bb092df7
imaakarsh <aakarsh027@outlook.com>
2026-06-20 16:43:07 +0530

    refactor: update variable declarations and enhance comments in script.js for clarity and educational purposes

--- JavaScriptEverything/script.js
+++ JavaScriptEverything/script.js
@@ -1,12 +1,12 @@
 // var let const - line by line 
 
-// var a
+// var a                   var use nhi krna 
 // var a = 2;
 // let a;
 // let a=23
-// var UserName = "Aakarsh"
+var UserName = "Aakarsh"
 // a = 23;
-// console.log(UserName);
+console.log(UserName);
 // console.log(a);
 
 // declaraton and initialization
@@ -18,11 +18,61 @@
 
 // app fhirse decelar kr skte ho error nhi aayega
 
-// var a = 13;
-// let a = 13;
+// var e  = 13;
+let f = 13; 
+var e = 23; // possible with var
+// let f = 23; // not possible with let      
 
 // Scope (global,block,functional)
-
-
 // reassignemet , redeclaration (possible with var)
+
+
+
+// Arithmetic operators 
+// + - * / % **
+
+// comparison operators
+// = , += , -= , *= , /= , %= , **= , > , < , >= , <=
+// === strict check
+
+
+12 !=13 // true
+// hasan != "hasan" // true 
+
+// logical operators
+// && , || , !
+// && and
+// || or
+// ! not
+
+
+/*
+let x = 10;
+x+=10;
+console.log(x);
+x-=10;
+console.log(x);
+x*=10;
+console.log(x);
+x/=10;
+console.log(x);
+x%=10;
+console.log(x);
+x**=10;
+console.log(x);
+*/  
+
+// conditional statements
+
+true && true // true && and
+true && false // false
+false && true // false
+false && false // false
+true || true // true || or
+true || false // true
+false || true // true || or
+false || false // false
+!true // false
+!false // true
+
 
