81287bbbdcfade9e1b29e22bedace26dc5b4c574
imaakarsh <aakarsh027@outlook.com>
2026-08-15 19:20:12 +0530

    ..

--- Part 2 JS/script.js
+++ Part 2 JS/script.js
@@ -1,9 +1,49 @@
 // dom = body
+// htlm se element select krna 
 // dom manipulation
 // text badlna 
 // html badlna
 // css badlna
 // attribute
-// event listeners
+// event listeners 
 
-fff
+
+// let abcd = document.getElementById("abcd");
+// console.dir(abcd);
+
+
+// let h1 = document.querySelector("h1");
+// h1.textContent = "hahaha";
+// console.dir(h1);
+
+// let a = document.querySelector("a")
+// a.href = "https://google.com" ; 
+// console.dir(a);
+
+// let a = document.querySelector("a");
+// console.log( a.getAttribute("herf"));
+
+
+// let a  = document.querySelector("a");
+// a.removeAttribute("herf")
+
+
+// create element
+// append/prepend kro jaha bhi chaiye element waha
+// let h1 = document.createElement("h1");
+// h1.textContent = "Hello ji";
+// console.dir(document.body.append("h1"));
+
+
+let h1 = document.querySelector("h1");
+// h1.style.color = "red";
+h1.style.backgroundColor = "pink";
+h1.style.fontFamily = "Gilroy"
+h1.style.textTransform = "capitalize"
+
+h1.classList.remove("hululu");
+
+// let h = document.querySelectorAll(".buy");
+// h.style.backgroundColor = "red";
+//  body.style.backgroundColor = "red";
+
